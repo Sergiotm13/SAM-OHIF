4b49f56933e1c07bc53c34549ea100ef64f4acd9
Sergiotm13 <sergiotores44@gmail.com>
2024-04-17 11:54:07 +0200

    Incluidos tipos para los puntos y las cajas y un tipo nuevo de icono para las cajas

---
+++ extensions/sam-segmentation/src/types/types.d.ts
@@ -0,0 +1,15 @@
+export interface Box {
+  rect: {
+    startX: number;
+    startY: number;
+    width: number;
+    height: number;
+  }[];
+}
+
+export interface Point {
+  point: {
+    x: number;
+    y: number;
+  };
+}
